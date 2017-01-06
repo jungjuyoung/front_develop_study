c2cf870db9d622b4219c66e59e8a3430a1ac9b31
Genji(KimSuho) <suhokim@coupang.com>
2017-01-06 23:02:07 +0900

    161218 과제

---
+++ kimsuho/homework/161218.js
@@ -0,0 +1,52 @@
+/*
+a(5) === '입력된 값은  number 입니다.'
+a('dddd') === '입력된 값은 string 입니다'
+a() === '알수없는 값입니다'
+*/
+
+/*
+max(1, 5) === 5
+max(100, 20) === 100
+max(30, 30) === 30
+*/
+
+/*
+min(1, 5, 12, 10) === 1
+min(100, 20, 15, 40) === 15
+min(30, 25, 25, 50) === 25
+*/
+
+
+/*
+exact('안녕', '안녕') === true
+exact('안녕', '메롱') === false
+*/
+
+/*
+abs(-400) === 400
+abs(200) === 200
+*/
+
+/*
+circle(5) === 78.5
+circle(70) === 15386
+*/
+
+/*
+circle( ? ) === ?
+*/
+
+
+/*
+createElement('div', '안녕하세요') === '<div>안녕하세요</div>'
+createElement('p', '반갑습니다') === '<p>반갑습니다</p>'
+createElement('h1') === '<h1></h1>'
+*/
+
+/*
+createElement( ? ) === '<div><p>반갑습니다</p></div>'
+*/
+
+/*
+makeURL('naver.com') === 'http://www.naver.com'
+*/
