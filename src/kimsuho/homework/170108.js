a436aa9ef7bbe498d22e8cb265b7ada896dac6e6
Genji(KimSuho) <suhokim@coupang.com>
2017-01-08 18:24:24 +0900

    template

--- src/kimsuho/homework/170108.js
+++ src/kimsuho/homework/170108.js
@@ -19,4 +19,12 @@ describe('콜백함수를 익히자', function() {
 		  })
 		).toBe(2);
 	});
+
+	it('반복되는 데이터를 마크업으로 변환한다', function() {
+		expect(
+		  template(inventory, function(fruit) {
+			  return '<li>'+fruit.name+'<span>수량 : '+fruit.quantity +'</span></li>';
+		  })
+		).toBe('<li>apples<span>수량 : 2</span></li><li>bananas<span>수량 : 0</span></li><li>cherries<span>수량 : 5</span></li><li>grapes<span>수량 : 10</span></li><li>strawberries<span>수량 : 8</span></li>')
+	});
 });
